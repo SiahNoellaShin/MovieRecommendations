e77c2e47f80eb629ce3a76a04653353432f67844
SiahNoellaShin <siah.noella.shin2027@gmail.com>
2024-07-24 10:18:47 +0900

    Add files via upload

---
+++ header (1).py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+def create_header():
+  st.title("Recommending Movies")
+  st.header("Made by Siah Noella Shin")
+  st.subheader("Enter your favorite movies to find other movies ")
+  ### FILL IN YOUR CODE FOR TITLE, NAME, AND DESCRIPTION HERE
+  pass
